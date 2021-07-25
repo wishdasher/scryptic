2982b45130c078c15d733e803cd7414784f7923d
Sophie Mori <ksmori14@gmail.com>
2021-07-25 12:36:39 -0400

    thing

--- games/spelling_bee.py
+++ games/spelling_bee.py
@@ -1,7 +1,7 @@
 import sys
 import re
 
-letters = sys.argv[2]
+letters = sys.argv[1]
 
 assert len(letters) == 7 and len(set(letters)) == 7
 
@@ -9,7 +9,7 @@ pattern = re.compile("^[" + letters + "]{4,}$")
 
 print(pattern)
 
-with open(sys.argv[1]) as f:
+with open('enable1.txt') as f:
     words = [w.strip() for w in f.readlines() if pattern.match(w.strip()) and letters[0] in w and len(w.strip()) >= 4]
 
 print("\n".join(words))
